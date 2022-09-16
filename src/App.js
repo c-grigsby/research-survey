25f254e96398da3bfb6ed0dcbd6df3573d9cf17d
Christopher Grigsby <grigsby.c@outlook.com>
2022-09-16 08:06:10 -0400

    feat(mobile): mod responsive (#3)

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import React from 'react';
 import { Card, CardBody, Container } from 'reactstrap';
 // @scripts
 import './App.css';
-import Survey from './Components/Survey/Survery';
+import SurveyForm from './Components/Survey/SurveryForm';
 
 function App() {
   return (
     <div className='App'>
-      <Container>
-        <Card className='form'>
-          <CardBody>
-            <Survey />
+      <Container className='Container'>
+        <Card className='Card'>
+          <CardBody className='CardBody'>
+            <div>
+              <SurveyForm className='Survey' />
+            </div>
           </CardBody>
         </Card>
       </Container>
